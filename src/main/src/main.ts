91793f1ec19a25014fd2ebd765ff7e4c6241f00e
sontu <tranuyson@outlook.com>
2023-05-05 15:01:55 +0700

    - complete setting feature

--- src/main/src/main.ts
+++ src/main/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { createElectronApp } from './electron'
+// import { createElectronApp } from './electron'
 import { MicroserviceOptions } from '@nestjs/microservices'
 import { ElectronIPCTransport } from 'nestjs-electron-ipc-transport'
 async function bootstrap() {
-  await createElectronApp()
+  // await createElectronApp()
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     strategy: new ElectronIPCTransport()
   })
