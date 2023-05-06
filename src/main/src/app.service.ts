b354c2b3e72997a9b1019a33b3474fdadf898f41
sontu <tranuyson@outlook.com>
2023-05-06 09:17:40 +0700

    -complete laser detection and hit point predictions

--- src/main/src/app.service.ts
+++ src/main/src/app.service.ts
@@ -7,18 +7,65 @@ export class AppService {
   getHello(): string {
     return 'Hello World!'
   }
-  detectSpot(imageBase64: string): string {
+  detectSpot({
+    imageBase64,
+    size,
+    distance
+  }: {
+    imageBase64: string
+    size: number
+    distance: number
+  }) {
     const base64data = imageBase64
       .replace('data:image/jpeg;base64', '')
       .replace('data:image/png;base64', '')
     const image = cv.imdecode(Buffer.from(base64data, 'base64'))
     const hsvImage = image.cvtColor(cv.COLOR_BGR2HSV)
-    const mask0 = hsvImage.inRange(new cv.Vec3(179, 50, 50), new cv.Vec3(180, 255, 255))
-
+    const mask0 = hsvImage.inRange(new cv.Vec3(120, 50, 50), new cv.Vec3(174, 255, 255))
+    image.drawCircle(
+      new cv.Point2(image.sizes[1] / 2, image.sizes[0] / 2),
+      180,
+      new cv.Vec3(0, 255, 255),
+      2,
+      cv.LINE_AA
+    )
     const { maxLoc } = mask0.minMaxLoc()
-    image.drawCircle(new cv.Point2(maxLoc.x, maxLoc.y), 20, new cv.Vec3(0, 255, 0), 2, cv.LINE_AA)
+    let detected = false
+    if (maxLoc.x !== 0 && maxLoc.y !== 0) {
+      detected = true
+      image.drawCircle(new cv.Point2(maxLoc.x, maxLoc.y), 10, new cv.Vec3(0, 255, 0), 2, cv.LINE_AA)
+
+      image.drawCircle(
+        new cv.Point2(
+          maxLoc.x,
+          maxLoc.y - (180 / Number(size)) * (distance === 25 ? 23.5 : distance === 20 ? 20 : 10)
+        ),
+        10,
+        new cv.Vec3(255, 0, 0),
+        2,
+        cv.LINE_AA
+      )
+    }
     const result = cv.imencode('.jpg', image).toString('base64')
-    this.electronService.send('signal', 'start')
+    return {
+      result,
+      detected
+    }
+  }
+  calibImage(imageBase64) {
+    const base64data = imageBase64
+      .replace('data:image/jpeg;base64', '')
+      .replace('data:image/png;base64', '')
+    const image = cv.imdecode(Buffer.from(base64data, 'base64'))
+    image.drawCircle(
+      new cv.Point2(image.sizes[1] / 2, image.sizes[0] / 2),
+      180,
+      new cv.Vec3(0, 255, 255),
+      2,
+      cv.LINE_AA
+    )
+
+    const result = cv.imencode('.jpg', image).toString('base64')
     return result
   }
 }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,7 +3,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
-
+import './index.scss'
+import { CssBaseline } from '@mui/material'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -14,6 +15,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    <CssBaseline />
     <ReactQueryDevtools />
   </QueryClientProvider>
 )
